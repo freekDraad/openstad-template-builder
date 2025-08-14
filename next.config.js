d3bc48fe522cd13ca8feb6c48da54d8990b59859
“freekDraad” <freek@draad.nu>
2025-08-14 10:47:24 +0200

    .

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: "/openstad-template-builder",
+  assetPrefix: "/openstad-template-builder",
 };
 
 module.exports = nextConfig;
